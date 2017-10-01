e2b367515818b7541dfece87ef7d639507b8552b
Youngbok Yoon <master@bluehack.net>
2017-10-01 18:20:34 +0900

    [BL-28] Added create_val_record.sh

---
+++ stylelens/object_detection/create_val_record.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source activate bl-magi
+export PYTHONPATH=$PYTHONPATH:`pwd`/../../tensorflow:`pwd`/../../slim
+export MODEL_BASE_PATH='gs://bluelens-style-model/object_detection'
+
+python create_tf_record.py \
+    --label_map_path=/dataset/deepfashion/data/label_map.pbtxt \
+    --data_dir=/dataset/deepfashion --folder=dataset --set=val \
+    --output_path=/dataset/data/val.record
