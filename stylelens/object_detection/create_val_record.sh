1376c14f2117e9d0c4f11ffdd7642f9b55f14af3
bluehackmaster <master@bluehack.net>
2017-10-01 18:36:18 +0900

    [BL-28] Changed output path of eval.record to gs

--- stylelens/object_detection/create_val_record.sh
+++ stylelens/object_detection/create_val_record.sh
@@ -7,4 +7,4 @@ export MODEL_BASE_PATH='gs://bluelens-style-model/object_detection'
 python create_tf_record.py \
     --label_map_path=/dataset/deepfashion/data/label_map.pbtxt \
     --data_dir=/dataset/deepfashion --folder=dataset --set=val \
-    --output_path=/dataset/data/val.record
+    --output_path=$MODEL_BASE_PATH/data/val.record
