47e755fd787a33f4f62349cbe3ec559f38b33c22
bluehackmaster <master@bluehack.net>
2017-10-01 13:30:36 +0900

    [BL-28] Changed label_map.pbtxt path

--- stylelens/object_detection/create_train_record.sh
+++ stylelens/object_detection/create_train_record.sh
@@ -5,6 +5,6 @@ export PYTHONPATH=$PYTHONPATH:`pwd`/../../tensorflow:`pwd`/../../slim
 export MODEL_BASE_PATH='gs://bluelens-style-model/object_detection'
 
 python create_tf_record.py \
-    --label_map_path=$MODEL_BASE_PATH/data/label_map.pbtxt \
+    --label_map_path=/dataset/deepfashion/data/label_map.pbtxt \
     --data_dir=/dataset/deepfashion --folder=dataset --set=train \
     --output_path=$MODEL_BASE_PATH/data/train.record
